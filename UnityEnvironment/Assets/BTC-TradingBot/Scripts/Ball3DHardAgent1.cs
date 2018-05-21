0778c247aeedd0e85434953cbd7ad6bac3081ecf
Icrodiscfe <33602499+Icrodiscfe@users.noreply.github.com>
2018-05-21 13:12:24 +0200

    Added current project state

---
+++ UnityEnvironment/Assets/BTC-TradingBot/Scripts/Ball3DHardAgent1.cs
@@ -0,0 +1,153 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball3DHardAgent1 : Agent
+{
+    [Header("Specific to Ball3DHard")]
+    public Chart chart;
+
+    public float bitcoins;
+    public float bitcoinsMax;
+    public float cryptos;
+    public float cryptosMax;
+    public float avarageCost;
+
+    float nomralizedChartValue = 0.5f;
+    float normalizedBitcoins;
+    float normalizedCryptos;
+
+    class MyTrade
+    {
+        public float Amount;    // getradete BTC Menge
+        public float Cost;  // BTC Preis
+    }
+
+    List<MyTrade> MyTrades = new List<MyTrade>(0);
+
+
+    public override void CollectObservations()
+    {
+        nomralizedChartValue = (chart.ChartValue - chart.ChartValueChange24hMin) / (chart.ChartValueChange24hMax - chart.ChartValueChange24hMin);
+        AddVectorObs(nomralizedChartValue);
+
+        bitcoinsMax = Mathf.Max(bitcoinsMax, bitcoins);
+        if (bitcoinsMax == 0)
+            normalizedBitcoins = 0f;
+        else
+            normalizedBitcoins = bitcoins / bitcoinsMax;
+        AddVectorObs(normalizedBitcoins);
+
+        cryptosMax = Mathf.Max(cryptosMax, cryptos);
+        if (cryptosMax == 0)
+            normalizedCryptos = 0f;
+        else
+            normalizedCryptos = cryptos / cryptosMax;
+        AddVectorObs(normalizedCryptos);
+
+        //AddVectorObs(avarageCost);
+    }
+
+    public override void AgentAction(float[] vectorAction, string textAction)
+    {
+
+        if (brain.brainParameters.vectorActionSpaceType == SpaceType.continuous && chart.Ready)
+        {
+            float action_trade = Mathf.Clamp(vectorAction[0], -1f, 1f);
+            float action_valueBitcoin = Mathf.Clamp(vectorAction[1], 0f, bitcoins);
+            float action_valueCrypto = Mathf.Clamp(vectorAction[2], 0f, cryptos);
+            
+            // Buy Cryptos
+            if (action_trade > 0)
+                if (action_valueBitcoin > 0)
+                {
+                    if (nomralizedChartValue > 0.5)
+                        AddReward(0.1f * Mathf.InverseLerp(0.5f, 1f, nomralizedChartValue));
+
+                    chart.BuyCrypto(action_valueBitcoin, chart.ChartValue, this);
+                    MyTrade trade = new MyTrade();
+                    trade.Amount = action_valueBitcoin / chart.ChartValue;
+                    trade.Cost = chart.ChartValue;
+                    MyTrades.Add(trade);
+                    GetAvarageTradesCost();
+                }
+
+            // Do nothing
+            if (action_trade > 0)
+            {
+                if (nomralizedChartValue > 0.8f)
+                    AddReward(0.1f * Mathf.InverseLerp(0.2f, 0f, normalizedBitcoins));
+
+                if (nomralizedChartValue < 0.2f)
+                    AddReward(0.1f * Mathf.InverseLerp(0.2f, 0f, normalizedCryptos));
+            }
+
+            // Buy Bitcoins
+            if (action_trade < 0)
+                if (action_valueCrypto > 0 && avarageCost > 0)
+                {
+                    if (nomralizedChartValue < 0.5)
+                        AddReward(0.1f * Mathf.InverseLerp(0.5f, 0f, nomralizedChartValue));
+
+                    chart.SellCrypto(action_valueCrypto, chart.ChartValue, this);
+                    UpdateTrades(action_valueCrypto);
+
+
+                }
+        }
+    }
+
+    public override void AgentReset()
+    {
+
+    }
+
+    void GetAvarageTradesCost()
+    {
+        if (MyTrades.Count == 0)
+        {
+            avarageCost = 0f;
+            return;
+        }
+
+        float avarage = 0f;
+        for (int i = 0; i < MyTrades.Count; i++)
+        {
+            avarage += MyTrades[i].Cost;
+        }
+        avarageCost = avarage / MyTrades.Count;
+    }
+
+    void UpdateTrades(float value)
+    {
+        float amount = value;
+        float lowestCost = 9000f;
+        int index = 0;
+
+        while (amount > 0)
+        {
+            if (MyTrades.Count == 0)
+                return;
+
+            for (int i = 0; i < MyTrades.Count; i++)
+            {
+                if (MyTrades[i].Cost < lowestCost)
+                {
+                    lowestCost += MyTrades[i].Cost;
+                    index = i;
+                }
+            }
+
+            if (MyTrades[index].Amount > amount)
+            {
+                MyTrades[index].Amount -= amount;
+            }
+            else
+            {
+                amount -= MyTrades[index].Amount;
+                MyTrades.Remove(MyTrades[index]);
+                GetAvarageTradesCost();
+            }
+        }
+    }
+}
---
+++ UnityEnvironment/Assets/BTC-TradingBot/Scripts/Chart.cs
@@ -0,0 +1,171 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using System.IO;
+
+public class Chart : MonoBehaviour {
+
+    public float ChartChange;
+    public float ChartValue = 50f;
+    public float ChartValueChange24h;
+    public float ChartValueChange24hMin;
+    public float ChartValueChange24hMax;
+    public float ChartValueChange1h;
+    public float PortfolioTotal;
+    public float PortfolioBTC;
+    public float PortfolioCRYPTOS;
+    public int TraderCount;
+    public bool Ready;
+
+    public List<Ball3DHardAgent1> MyAgents;
+
+    public List<Text> TextList;
+    List<Ball3DHardAgent1> listTrader = new List<Ball3DHardAgent1>(0);
+    
+    float startTime = 0f;
+    List<float> ListChartValue = new List<float>();
+
+
+	void Start () {
+		
+	}
+
+    float ChartTimer = 0f;
+
+    void Update () {
+        ChartTimer += Time.deltaTime;
+
+        if (ChartTimer >= 1f)
+        {
+            ChartChange = Random.Range(-1.0f, 1.0f);
+            if (Input.GetKey(KeyCode.DownArrow))
+                ChartChange = Random.Range(-1.0f, 0.0f);
+            if (Input.GetKey(KeyCode.UpArrow))
+                ChartChange = Random.Range(0.0f, 1.0f);
+
+            ChartValue += ChartChange;
+            ChartTimer = 0f;
+
+            ListChartValue.Add(ChartValue);
+
+            if (ListChartValue.Count >= 10)
+            {
+                if (ListChartValue.Count > 10)
+                    ListChartValue.RemoveAt(0);
+
+                float min = 9999f;
+                float max = 0f;
+
+                foreach (float i in ListChartValue)
+                {
+                    min = Mathf.Min(min, i);
+                    max = Mathf.Max(max, i);
+                }
+                ChartValueChange24hMin = min;
+                ChartValueChange24hMax = max;
+
+                ChartValueChange24h = 100 / ListChartValue[0] * (ListChartValue[9] - ListChartValue[0]);
+                ChartValueChange1h = 100 / ListChartValue[8] * (ListChartValue[9] - ListChartValue[8]);
+
+                foreach (Ball3DHardAgent1 Agent in MyAgents)
+                {
+                    Agent.RequestDecision();
+                }
+            }
+            
+
+            //StartCoroutine(GetText());
+        }
+
+        if (ChartValue <= 30)
+            ChartValue = 30;
+
+        var value = (ChartValue - ChartValueChange24hMin) / (ChartValueChange24hMax - ChartValueChange24hMin);
+
+        TextList[0].text = value.ToString();
+        TextList[1].text = ChartValue.ToString();
+        TextList[2].text = ChartValueChange24h.ToString();
+        TextList[3].text = PortfolioBTC.ToString();
+        TextList[4].text = PortfolioCRYPTOS.ToString();
+        TextList[5].text = PortfolioTotal.ToString();
+        TextList[6].text = ChartValueChange1h.ToString();
+
+        if (startTime >= 0)
+            startTime -= Time.deltaTime;
+
+        Ready = startTime < 0;
+
+        PortfolioBTC = 0;
+        PortfolioCRYPTOS = 0;
+
+        TraderCount = listTrader.Count;
+
+        for (int i = 0; i < listTrader.Count; i++)
+        {
+            PortfolioBTC += listTrader[i].bitcoins;
+            PortfolioCRYPTOS += listTrader[i].cryptos;
+        }
+
+        PortfolioTotal = PortfolioBTC + PortfolioCRYPTOS * ChartValue;
+
+        //CCP_API_Request myObject = new CCP_API_Request();
+        //myObject = JsonUtility.FromJson<CCP_API_Request>(json);
+    }
+
+    public void BuyCrypto(float amount, float cost, Ball3DHardAgent1 trader)
+    {
+        if (!listTrader.Contains(trader))
+            listTrader.Add(trader);
+
+        trader.cryptos += amount / cost;
+        trader.bitcoins -= amount;
+
+        LogTrade();
+    }
+
+    public void SellCrypto(float amount, float cost, Ball3DHardAgent1 trader)
+    {
+        if (!listTrader.Contains(trader))
+            listTrader.Add(trader);
+
+        trader.cryptos -= amount;
+        trader.bitcoins += amount * cost;
+
+        LogTrade();
+    }
+
+    void LogTrade ()
+    {
+        File.AppendAllText(Application.dataPath + "/MyLogfile.txt", ChartValue + ";" + PortfolioTotal + "\r\n");
+    }
+    
+    public class CCP_API_Request
+    {
+        public string symbol;   // "ETH"
+        public float price_btc; // "0.0841251"
+        public float percent_change_1h; // "-0.38"
+        public float percent_change_24h;    // "1.23"
+        public float percent_change_7d; // "2.81"
+        public int last_updated;    // "1526718258"
+    }
+
+    IEnumerator GetText()
+    {
+        using (UnityWebRequest www = UnityWebRequest.Get("https://api.binance.com/api/v3/ticker/price?symbol=ETHBTC"))
+        {
+            yield return www.Send();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                // Show results as text
+                Debug.Log(www.downloadHandler.text);
+            }
+        }
+    }
+}
